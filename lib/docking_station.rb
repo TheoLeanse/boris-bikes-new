63c1ba8ab04da0016652293e77cbe3a0b7a80b35
TheoLeanse <theoleanse@gmail.com>
2015-06-11 18:56:47 +0100

    modifies DockingStation#release_bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -17,11 +17,21 @@ class DockingStation
 
   def release_bike
     fail 'No bikes available' if empty?
-    fail 'No working bikes available' if bikes.last.broken?
-    bikes.pop
+  #  if there is a bike which is working, pop it and then stop
+    bikes.each do |bike|
+      if bike.working?
+        return bikes.delete(bike) #do something
+      else
+        fail "No working bikes available"
+      end
+    end
+
+    # fail 'No working bikes available' if bikes.last.broken?
+    # bikes.pop
   end
 
   private
+  
   attr_reader :bikes
 
   def full?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,13 +9,26 @@ describe DockingStation do
       end
 
     it 'raises an error when there are no working bikes available' do
-        bike = Bike.new
-        bike.report_broken
-        subject.dock bike
-        expect { subject.release_bike }.to raise_error 'No working bikes available'
-      end
+      bike = Bike.new
+      bike.report_broken
+      subject.dock bike
+      expect { subject.release_bike }.to raise_error 'No working bikes available'
     end
 
+    it 'does not raise an error when there are any working bikes available' do
+      bike = Bike.new
+      bike2 = Bike.new
+      bike2.report_broken
+      subject.dock bike
+      subject.dock bike2
+      expect { subject.release_bike }.not_to raise_error
+    end
+
+    # it releases only working bike when there are working bikes in the array
+    #
+    # ... and it deletes that working bike from the array
+
+end
     describe 'dock' do
       it 'raises an error when full' do
         subject.capacity.times { subject.dock Bike.new }
