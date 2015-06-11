2a063968a8044af47f7433fb2e1b6d6d13291f04
Jonathan Ng <jonngkh@hotmail.com>
2015-06-11 14:13:24 +0100

    added default capacity functionality

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,35 @@
+require_relative 'bike'
 
 class DockingStation
+
+  attr_reader :capacity
+  DEFAULT_CAPACITY = 20
+
+  def initialize
+    @bikes = []
+    @capacity = DEFAULT_CAPACITY
+  end
+
   def dock bike
-    @bike = bike
+    fail 'Docking station full' if full?
+
+    @bikes << bike
   end
 
   def release_bike
-    fail 'No bikes available' if !@bike
-    @bike
+    fail 'No bikes available' if empty?
+
+    @bikes.pop
+  end
+
+  private
+
+  def full?
+    @bikes && @bikes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
